97392e5dd09b31bb63e96c028b913c668f78ca6b
sofiane <sofyanekebci@gmail.com>
2023-05-05 16:13:30 +0200

    On a la possibilité de changer le nom de notre animal

--- app/src/main/java/com/example/hobbyzooapp/ActivityPage.java
+++ app/src/main/java/com/example/hobbyzooapp/ActivityPage.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,6 +16,11 @@ public class ActivityPage extends AppCompatActivity {
 
     ImageView petPic;
     TextView petName;
+    Button editNamePetButton;
+    EditText editTextPetName;
+    Button validatePetNAme;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +30,13 @@ public class ActivityPage extends AppCompatActivity {
         petName = findViewById(R.id.activityPagePetName);
         petName.setText("Coco");
         petPic.setImageResource(R.drawable.koa);
+        editNamePetButton=findViewById(R.id.activityPageEditPetNameButton);
+        editTextPetName = findViewById(R.id.activityPagePetNameEdit);
+        InputFilter[] filters = new InputFilter[1];
+        filters[0] = new InputFilter.LengthFilter(10);
+        editTextPetName.setFilters(filters);
 
+        validatePetNAme = findViewById(R.id.activityPagecheckPetNameButton);
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.koa);
         /*
         int newWidth = (int) (bitmap.getWidth() * (70 / 100.0));
@@ -30,6 +45,33 @@ public class ActivityPage extends AppCompatActivity {
         petPic.setScaleType(ImageView.ScaleType.CENTER_CROP);*/
 
         petPic.setImageBitmap(bitmap);
+
+        editNamePetButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                editTextPetName.setText(petName.getText());
+                editTextPetName.setVisibility(View.VISIBLE);
+                editNamePetButton.setVisibility(View.GONE);
+                validatePetNAme.setVisibility(View.VISIBLE);
+                petName.setVisibility(View.GONE);
+            }
+        });
+
+        validatePetNAme.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                editTextPetName.setVisibility(View.GONE);
+                String newPetName = String.valueOf(editTextPetName.getText());;
+                petName.setText(newPetName);
+                editNamePetButton.setVisibility(View.VISIBLE);
+                validatePetNAme.setVisibility(View.GONE);
+                petName.setVisibility(View.VISIBLE);
+            }
+        });
+
+
 
     }
 

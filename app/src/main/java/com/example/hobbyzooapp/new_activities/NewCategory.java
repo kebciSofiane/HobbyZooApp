b81911b4ffa6a431a38c29feaceb581e6795e4b9
clemf <clem.fresnel@gmail.com>
2023-05-05 22:04:21 +0200

    creation des classes

---
+++ app/src/main/java/com/example/hobbyzooapp/new_activities/NewCategory.java
@@ -0,0 +1,19 @@
+package com.example.hobbyzooapp.new_activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.hobbyzooapp.R;
+
+
+public class NewCategory extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_category);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/hobbyzooapp/new_activities/NewSession.java
@@ -0,0 +1,16 @@
+package com.example.hobbyzooapp.new_activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.hobbyzooapp.R;
+
+public class NewSession extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_session);
+    }
+}

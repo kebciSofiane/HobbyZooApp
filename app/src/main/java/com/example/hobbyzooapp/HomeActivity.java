2024c0e7dea66f7b5e795028d7eb683922e8c870
r20031646 <ramdani.meriem2001@gmail.com>
2023-05-09 17:50:04 +0200

    page home modified: bar to buttons

--- app/src/main/java/com/example/hobbyzooapp/HomeActivity.java
+++ app/src/main/java/com/example/hobbyzooapp/HomeActivity.java
@@ -10,6 +10,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -22,6 +24,8 @@ public class HomeActivity extends AppCompatActivity {
 
     ActionBar actionBar;
 
+    ImageButton calendarBtn, runBtn, profileBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,51 +36,79 @@ public class HomeActivity extends AppCompatActivity {
 
         firebaseAuth = FirebaseAuth.getInstance();
 
+        //buttons
+
+        calendarBtn = findViewById(R.id.calendar_btn);
+        runBtn = findViewById(R.id.run_btn);
+        profileBtn = findViewById(R.id.profile_btn);
+
+        calendarBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this,CalendarActivity.class));
+
+            }
+        });
+        runBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this,CalendarActivity.class));
+
+            }
+        });
+        profileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this,CalendarActivity.class));
+
+            }
+        });
+
         //bottom navigation
-        BottomNavigationView navigationView = findViewById(R.id.navigation);
-        navigationView.setOnNavigationItemSelectedListener(selectedListener);
-
-        //
-        actionBar.setTitle("Home");
-        HomeFragment fragment = new HomeFragment();
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.content,fragment,"");
-        ft.commit();
+//        BottomNavigationView navigationView = findViewById(R.id.navigation);
+//        navigationView.setOnNavigationItemSelectedListener(selectedListener);
+//
+//        //
+//        actionBar.setTitle("Home");
+//        HomeFragment fragment = new HomeFragment();
+//        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+//        ft.replace(R.id.content,fragment,"");
+//        ft.commit();
 
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener selectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()){
-
-                case R.id.calendar:
-                    actionBar.setTitle("Calendar");
-                    CalendarFragment fragment1 = new CalendarFragment();
-                    FragmentTransaction ft1 = getSupportFragmentManager().beginTransaction();
-                    ft1.replace(R.id.content,fragment1,"");
-                    ft1.commit();
-                    return true;
-                case R.id.nav_runsession:
-                    actionBar.setTitle("Run Session");
-                    RunsessionFragment fragment2 = new RunsessionFragment();
-                    FragmentTransaction ft2 = getSupportFragmentManager().beginTransaction();
-                    ft2.replace(R.id.content,fragment2,"");
-                    ft2.commit();
-
-                    return true;
-                case R.id.nav_profile:
-                    actionBar.setTitle("Profile");
-                    ProfileFragment fragment3 = new ProfileFragment();
-                    FragmentTransaction ft3 = getSupportFragmentManager().beginTransaction();
-                    ft3.replace(R.id.content,fragment3,"");
-                    ft3.commit();
-                    return true;
-        }
-        return false;
-    }
-
-    };
+//    private BottomNavigationView.OnNavigationItemSelectedListener selectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+//        @Override
+//        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//            switch (item.getItemId()){
+//
+//                case R.id.calendar:
+//                    actionBar.setTitle("Calendar");
+//                    CalendarFragment fragment1 = new CalendarFragment();
+//                    FragmentTransaction ft1 = getSupportFragmentManager().beginTransaction();
+//                    ft1.replace(R.id.content,fragment1,"");
+//                    ft1.commit();
+//                    return true;
+//                case R.id.nav_runsession:
+//                    actionBar.setTitle("Run Session");
+//                    RunsessionFragment fragment2 = new RunsessionFragment();
+//                    FragmentTransaction ft2 = getSupportFragmentManager().beginTransaction();
+//                    ft2.replace(R.id.content,fragment2,"");
+//                    ft2.commit();
+//
+//                    return true;
+//                case R.id.nav_profile:
+//                    actionBar.setTitle("Profile");
+//                    ProfileFragment fragment3 = new ProfileFragment();
+//                    FragmentTransaction ft3 = getSupportFragmentManager().beginTransaction();
+//                    ft3.replace(R.id.content,fragment3,"");
+//                    ft3.commit();
+//                    return true;
+//        }
+//        return false;
+//    }
+//
+//    };
 
     private void checkUserStatus(){
         FirebaseUser user = firebaseAuth.getCurrentUser();

8c6eef637893e84dd78101ad1d61809573babac1
f20008012 <clem.fresnel@gmail.com>
2023-05-10 16:04:23 +0200

    test db

--- app/src/main/java/com/example/hobbyzooapp/MainActivity.java
+++ app/src/main/java/com/example/hobbyzooapp/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     }
 }

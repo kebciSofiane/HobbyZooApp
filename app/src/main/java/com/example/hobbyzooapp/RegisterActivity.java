e42ad64d62c1056b0e8c46466e52e18ea2154965
r20031646 <ramdani.meriem2001@gmail.com>
2023-05-10 14:04:43 +0200

    Add username (pseudo) to the database

--- app/src/main/java/com/example/hobbyzooapp/RegisterActivity.java
+++ app/src/main/java/com/example/hobbyzooapp/RegisterActivity.java
@@ -6,30 +6,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 
 import java.util.HashMap;
 
@@ -39,7 +32,7 @@ public class RegisterActivity extends AppCompatActivity  {
     EditText emailEt, passwordEt, pseudoET;
     Button registerBtn;
     TextView haveAccountTv;
-    ImageView photoV;
+
 
     ProgressDialog progressDialog;
     private FirebaseAuth auth;
@@ -59,10 +52,9 @@ public class RegisterActivity extends AppCompatActivity  {
 
         emailEt = findViewById(R.id.emailEt);
         passwordEt = findViewById(R.id.passwordEt);
-        pseudoET = findViewById(R.id.pseudoET);
         registerBtn = findViewById(R.id.register_btn);
         haveAccountTv = findViewById(R.id.have_accountTv);
-        photoV = findViewById(R.id.photoIV);
+        pseudoET =  findViewById(R.id.pseudoET);
 
         auth =FirebaseAuth.getInstance();
 
@@ -75,7 +67,6 @@ public class RegisterActivity extends AppCompatActivity  {
                 String email = emailEt.getText().toString().trim();
                 String password = passwordEt.getText().toString().trim();
                 String pseudo = pseudoET.getText().toString().trim();
-
                 if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
                     emailEt.setError("Invalid email address");
                     emailEt.setFocusable(true);
@@ -85,7 +76,7 @@ public class RegisterActivity extends AppCompatActivity  {
                     passwordEt.setFocusable(true);
                 }
                 else{
-                   registerUser(email, password,pseudo,photoV);
+                    registerUser(email, password,pseudo);
                 }
             }
         });
@@ -100,8 +91,7 @@ public class RegisterActivity extends AppCompatActivity  {
     }
 
 
-
-    private void registerUser(String email, String password, String pseudo, Uri imageUri) {
+    private void registerUser(String email, String password,String pseudo) {
         progressDialog.show();
         auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -115,45 +105,17 @@ public class RegisterActivity extends AppCompatActivity  {
                             //get user uid and email from auth
                             String email = user.getEmail();
                             String uid = user.getUid();
-                            String pseudo = user.getDisplayName();
+
+                            //get user pseudo from the pseudoET field
+                            String pseudo = pseudoET.getText().toString().trim();
 
                             //when user is registered, store user info in firebase realtime database
-                            HashMap<String, Object> hashMap = new HashMap<>();
+                            HashMap<Object, String> hashMap = new HashMap<>();
 
                             hashMap.put("email", email);
                             hashMap.put("uid", uid);
                             hashMap.put("pseudo", pseudo);
-
-                            // upload profile image to Firebase Storage
-                            if (imageUri != null) {
-                                StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("users/" + uid + "/profile.jpg");
-
-                                storageReference.putFile(imageUri)
-                                        .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                                            @Override
-                                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                                                // get download url of uploaded image
-                                                Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
-                                                while (!uriTask.isSuccessful()) ;
-                                                Uri downloadUri = uriTask.getResult();
-
-                                                if (downloadUri != null) {
-                                                    // update user profile with profile image url
-                                                    UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                                                            .setPhotoUri(downloadUri)
-                                                            .build();
-                                                    user.updateProfile(profileUpdates);
-                                                    hashMap.put("image", downloadUri.toString());
-                                                }
-                                            }
-                                        })
-                                        .addOnFailureListener(new OnFailureListener() {
-                                            @Override
-                                            public void onFailure(@NonNull Exception e) {
-                                                // handle image upload failure
-                                            }
-                                        });
-                            }
+                            hashMap.put("image", "");
 
                             FirebaseDatabase database = FirebaseDatabase.getInstance();
 
@@ -177,16 +139,15 @@ public class RegisterActivity extends AppCompatActivity  {
                         Toast.makeText(RegisterActivity.this,""+e.getMessage(),Toast.LENGTH_SHORT).show();
                     }
                 });
+
     }
 
 
-
-
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-            int id = item.getItemId();
-            if(id == android.R.id.home){
-                this.finish();
-            }
+        int id = item.getItemId();
+        if(id == android.R.id.home){
+            this.finish();
+        }
 
         return super.onOptionsItemSelected(item);
     }

9b38ff11dfdf38eda1fe7e86b34f8c09d6407808
d21218463 <juliette.dadoit@etu.univ-amu.fr>
2023-05-16 16:33:14 +0200

    bdd todo list pas testee

--- app/src/main/java/com/example/hobbyzooapp/Activities/ActivityPage.java
+++ app/src/main/java/com/example/hobbyzooapp/Activities/ActivityPage.java
@@ -14,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -25,8 +26,10 @@ import com.example.hobbyzooapp.Sessions.ListSessionsAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -101,14 +104,19 @@ public class ActivityPage extends AppCompatActivity {
                     String activityId = snapshot.child("activityId").getValue(String.class);
                     String taskName  = snapshot.child("taskName").getValue(String.class);
                     String taskStatus = snapshot.child("taskStatus").getValue(String.class);
-                    if (activityId==thisActivityId){
-                        if (taskStatus=="FALSE"){
+                    if (activityId.equals(thisActivityId)){
+                        if (taskStatus.equals("FALSE")){
                             todoList.add(new TodoTask(taskName, Boolean.FALSE));
                         }else{
                             todoList.add(new TodoTask(taskName, Boolean.TRUE));
                         }
                     }
                 }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
 

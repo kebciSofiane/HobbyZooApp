c96ca7e41b04f5560f82488e9124ea66652901a9
r20031646 <ramdani.meriem2001@gmail.com>
2023-05-17 15:31:52 +0200

    SettingsActivity + logout

--- app/src/main/java/com/example/hobbyzooapp/SettingsActivity.java
+++ app/src/main/java/com/example/hobbyzooapp/SettingsActivity.java
@@ -1,16 +1,94 @@
 package com.example.hobbyzooapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.example.hobbyzooapp.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+
+    private Button notificationsButton;
+    private Button termsButton;
+    private Button rateButton;
+    private Button helpButton;
+    private Button aboutButton;
+    private Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        notificationsButton = findViewById(R.id.notificationsBtn);
+        termsButton = findViewById(R.id.termsBtn);
+        rateButton = findViewById(R.id.rateBtn);
+        helpButton = findViewById(R.id.helpBtn);
+        aboutButton = findViewById(R.id.aboutBtn);
+        logoutButton = findViewById(R.id.logoutBtn);
+
+        notificationsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Logique pour gérer le clic sur le bouton "Notifications"
+            }
+        });
+        termsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Logique pour gérer le clic sur le bouton "Terms of Service"
+            }
+        });
+
+        rateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Logique pour gérer le clic sur le bouton "Rate HobbyZoo"
+            }
+        });
+
+        helpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Logique pour gérer le clic sur le bouton "Help"
+            }
+        });
+
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Logique pour gérer le clic sur le bouton "About"
+            }
+        });
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signOut();
+                checkUserStatus();
+            }
+        });
+
     }
+    private void checkUserStatus() {
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+            // Utilisateur connecté, redirigez-le vers HomeActivity
+            startActivity(new Intent(SettingsActivity.this, HomeActivity.class));
+        } else {
+            // Utilisateur non connecté, redirigez-le vers RegistrationOrConnexion
+            startActivity(new Intent(SettingsActivity.this, RegistrationOrConnexion.class));
+        }
+        finish();
+    }
+
 }

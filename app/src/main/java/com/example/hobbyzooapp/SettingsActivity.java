e0af953004eaf3cd815317ccd825c9a94d40ed46
r20031646 <ramdani.meriem2001@gmail.com>
2023-05-17 15:54:06 +0200

    backbtn works

--- app/src/main/java/com/example/hobbyzooapp/SettingsActivity.java
+++ app/src/main/java/com/example/hobbyzooapp/SettingsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,6 +22,7 @@ public class SettingsActivity extends AppCompatActivity {
     private Button helpButton;
     private Button aboutButton;
     private Button logoutButton;
+    private ImageButton backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +37,7 @@ public class SettingsActivity extends AppCompatActivity {
         helpButton = findViewById(R.id.helpBtn);
         aboutButton = findViewById(R.id.aboutBtn);
         logoutButton = findViewById(R.id.logoutBtn);
+        backBtn = findViewById(R.id.backButton);
 
         notificationsButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,6 +70,14 @@ public class SettingsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Logique pour gérer le clic sur le bouton "About"
+            }
+        });
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SettingsActivity.this, ProfileActivity.class));
+
             }
         });
 

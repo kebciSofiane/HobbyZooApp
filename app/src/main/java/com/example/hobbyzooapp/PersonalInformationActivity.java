4add4e6dfa8c9d3c707b02ac88f2355a23527380
r20031646 <ramdani.meriem2001@gmail.com>
2023-05-25 13:52:17 +0200

    a bug fixed in activity_personal_information.xml

--- app/src/main/java/com/example/hobbyzooapp/PersonalInformationActivity.java
+++ app/src/main/java/com/example/hobbyzooapp/PersonalInformationActivity.java
@@ -6,7 +6,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;

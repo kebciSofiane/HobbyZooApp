85392e0af3492bbccd344eb5a11d534d169cbc83
Lina <lina.aitrazzouk@etu.univ-amu.fr>
2023-05-25 17:05:32 +0200

    première version calendrier avec couleur

--- app/src/main/java/com/example/hobbyzooapp/Calendar/CalendarAdapter.java
+++ app/src/main/java/com/example/hobbyzooapp/Calendar/CalendarAdapter.java
@@ -1,16 +1,18 @@
 package com.example.hobbyzooapp.Calendar;
 import com.example.hobbyzooapp.R;
 import com.example.hobbyzooapp.Sessions.MyDailySessions;
+import com.example.hobbyzooapp.Sessions.Session;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.firestore.CollectionReference;
+
 
 import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,7 +24,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.concurrent.atomic.AtomicMarkableReference;
+
+import android.util.Log;
+
 
 class CalendarAdapter extends RecyclerView.Adapter<CalendarViewHolder>
 {
@@ -60,79 +64,24 @@ class CalendarAdapter extends RecyclerView.Adapter<CalendarViewHolder>
         else
         {
             holder.dayOfMonth.setText(String.valueOf(date.getDayOfMonth()));
-            if(date.equals(CalendarUtils.selectedDate))
-                holder.parentView.setBackgroundColor(Color.LTGRAY);
-            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            if(date.equals(CalendarUtils.selectedDate)){
+                GradientDrawable borderDrawable = new GradientDrawable();
+                borderDrawable.setShape(GradientDrawable.RECTANGLE);
+                borderDrawable.setCornerRadius(10);
+                borderDrawable.setStroke(4, Color.BLACK);
+                holder.parentView.setBackground(borderDrawable);
+
+                //holder.parentView.setBackgroundColor(Color.LTGRAY);
+            }
 
 
-///////////////////////////////////////////////
-           searchSession(date.getDayOfMonth(), date.getMonthValue(), date.getYear(), new SessionExistsCallback() {
-                @Override
-                public void onSessionExists(boolean sessionExists) {
-                    // Utilisez la valeur de sessionExists ici
-                    if (sessionExists) {
-                        holder.parentView.setBackgroundColor(Color.RED);
-                    } else {
+            getSessions(holder,date);
 
-                    }
 
-                }
-
-           });
         }
     }
 
 
-    public void searchSession(int jour, int mois, int annee, SessionExistsCallback callback) {
-        Query dayQuery = databaseRef.orderByChild("session_day").equalTo(jour);
-        Query monthQuery = databaseRef.orderByChild("session_month").equalTo(mois);
-        Query yearQuery = databaseRef.orderByChild("session_year").equalTo(annee);
-
-        dayQuery.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    monthQuery.addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(DataSnapshot dataSnapshot) {
-                            if (dataSnapshot.exists()) {
-                                yearQuery.addListenerForSingleValueEvent(new ValueEventListener() {
-                                    @Override
-                                    public void onDataChange(DataSnapshot dataSnapshot) {
-                                        boolean sessionExists = dataSnapshot.exists();
-                                        callback.onSessionExists(sessionExists);
-                                    }
-
-                                    @Override
-                                    public void onCancelled(DatabaseError databaseError) {
-                                        callback.onSessionExists(false);
-                                    }
-                                });
-                            } else {
-                                callback.onSessionExists(false);
-                            }
-                        }
-
-                        @Override
-                        public void onCancelled(DatabaseError databaseError) {
-                            callback.onSessionExists(false);
-                        }
-                    });
-                } else {
-                    callback.onSessionExists(false);
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                callback.onSessionExists(false);
-            }
-        });
-    }
-
-
-
-    /////////////////////////
 
     @Override
     public int getItemCount() {
@@ -142,4 +91,74 @@ class CalendarAdapter extends RecyclerView.Adapter<CalendarViewHolder>
     public interface  OnItemListener {
         void onItemClick(int position, LocalDate date);
     }
+
+
+
+    private void getSessions(CalendarViewHolder holder,LocalDate date) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference reference = database.getReference("Session");
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        String uid = user.getUid();
+        ArrayList<Session> mySessions = new ArrayList<>();
+        reference.orderByChild("user_id").equalTo(uid).addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String session_id = snapshot.child("session_id").getValue(String.class);
+                    String session_duration = snapshot.child("session_duration").getValue(String.class);
+                    String activity_id = snapshot.child("activity_id").getValue(String.class);
+                    String session_day = snapshot.child("session_day").getValue(String.class);
+                    String session_month = snapshot.child("session_month").getValue(String.class);
+                    String session_year = snapshot.child("session_year").getValue(String.class);
+                    String session_done = snapshot.child("session_done").getValue(String.class);
+
+
+
+
+                    DatabaseReference referenceActivity = database.getReference("Activity");
+
+                    referenceActivity.child(activity_id).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @RequiresApi(api = Build.VERSION_CODES.O)
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.exists()) {
+                                String activityName = dataSnapshot.child("activity_name").getValue(String.class);
+                                int hourDuration = Integer.parseInt(session_duration) / 60;
+                                int minutesDuration = Integer.parseInt(session_duration) % 60;
+                                LocalDate sessionDate = LocalDate.of(Integer.parseInt(session_year), Integer.parseInt(session_month), Integer.parseInt(session_day));
+
+                                if (date.getMonth() == sessionDate.getMonth() &&
+                                        date.getDayOfMonth() == sessionDate.getDayOfMonth()&&
+                                        date.getYear() == sessionDate.getYear()) {
+                                    GradientDrawable backgroundDrawable = new GradientDrawable();
+                                    backgroundDrawable.setShape(GradientDrawable.RECTANGLE);
+                                    backgroundDrawable.setCornerRadius(10); // Définissez ici le rayon de courbure des coins (en pixels)
+                                    backgroundDrawable.setColor(Color.RED);
+                                    holder.itemView.setBackground(backgroundDrawable);
+                                }
+                            } else {
+                                // L'activité n'existe pas dans la base de données
+                            }
+
+
+                        }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+                                // Une erreur s'est produite lors de la récupération des données
+                            }
+                        });
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("TAG", "Erreur lors de la récupération des données", databaseError.toException());
+            }
+        });
+    }
+
 }

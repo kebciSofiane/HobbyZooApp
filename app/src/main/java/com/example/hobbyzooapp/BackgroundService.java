ce4ea12d77beef365f8fe6b3c9fe34b41bc75324
r20031646 <ramdani.meriem2001@gmail.com>
2023-06-02 18:36:58 +0200

    notifications works with hours, but not minutes

--- app/src/main/java/com/example/hobbyzooapp/BackgroundService.java
+++ app/src/main/java/com/example/hobbyzooapp/BackgroundService.java
@@ -15,9 +15,6 @@ import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
 
-import com.example.hobbyzooapp.R;
-import com.example.hobbyzooapp.Sessions.MyDailySessions;
-import com.example.hobbyzooapp.Sessions.MyDailySessionsAdapter;
 import com.example.hobbyzooapp.Sessions.Session;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -27,7 +24,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.sql.Time;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Calendar;
 
@@ -41,56 +38,40 @@ public class BackgroundService extends Service {
 
     private Handler handler;
     private Runnable runnable;
+    Calendar calendar;
 
 
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onCreate() {
         super.onCreate();
         Notification notification = createNotification();
         startForeground(NOTIFICATION_ID, notification);
-
+//        getSessions();
+//
         handler = new Handler();
         runnable = new Runnable() {
             @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void run() {
                 // Vérifier si l'heure actuelle est 10 heures du matin
-                Calendar calendar = Calendar.getInstance();
-                int currentHour = calendar.get(Calendar.HOUR_OF_DAY);
-                int currentMinute = calendar.get(Calendar.MINUTE);
-               Log.d("abcd" , "" +currentHour);
+                calendar = Calendar.getInstance();
+                getSessions();
+                Log.d("abcd" , "" );
 
-                if (currentHour == 16) {
-                    // Votre logique pour vérifier le nombre de sessions du jour
-                    int sessionCount = getSessions().size();
 
-                    Log.d("count", "" + sessionCount);
-;                   if (sessionCount > 0) {
-                        // Envoyer la notification
-                        showSessionNotification(sessionCount);
-                    }
-                }
-
-                // Planifier la prochaine vérification pour le lendemain à 10 heures
-                calendar.add(Calendar.DAY_OF_YEAR, 1);
-                calendar.set(Calendar.HOUR_OF_DAY, 10);
-                long delayMillis = calendar.getTimeInMillis() - System.currentTimeMillis();
-                handler.postDelayed(this, delayMillis);
             }
         };
+
+        calendar = Calendar.getInstance();  // Initialiser la variable calendar ici
+        calendar.add(Calendar.DAY_OF_YEAR, 1);
+        calendar.set(Calendar.HOUR_OF_DAY, 10);
+        long delayMillis = calendar.getTimeInMillis() - System.currentTimeMillis();
+        handler.postDelayed(runnable, delayMillis);
     }
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        handler.post(runnable);
-        return START_STICKY;
-    }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        handler.removeCallbacks(runnable);
-    }
 
     @Nullable
     @Override
@@ -98,60 +79,52 @@ public class BackgroundService extends Service {
         return null;
     }
 
-    private ArrayList<Session> getSessions() {
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private ArrayList<String> getSessions() {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference reference = database.getReference("Session");
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
         FirebaseUser user = firebaseAuth.getCurrentUser();
         String uid = user.getUid();
-        ArrayList<Session> mySessions = new ArrayList<>();
+        ArrayList<String> mySessions = new ArrayList<>();
+        LocalDate localDate = LocalDate.now();
 
         reference.orderByChild("user_id").equalTo(uid).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    String session_id = snapshot.child("session_id").getValue(String.class);
-                    String session_duration = snapshot.child("session_duration").getValue(String.class);
-                    String activity_id = snapshot.child("activity_id").getValue(String.class);
                     String session_day = snapshot.child("session_day").getValue(String.class);
                     String session_month = snapshot.child("session_month").getValue(String.class);
                     String session_year = snapshot.child("session_year").getValue(String.class);
-                    String session_image = snapshot.child("session_picture").getValue(String.class);
-                    String session_done = snapshot.child("session_done").getValue(String.class);
 
-                    DatabaseReference referenceActivity = database.getReference("Activity");
-
-                    referenceActivity.child(activity_id).addListenerForSingleValueEvent(new ValueEventListener() {
-                        @RequiresApi(api = Build.VERSION_CODES.O)
-                        @Override
-                        public void onDataChange(DataSnapshot dataSnapshot) {
-                            if (dataSnapshot.exists()) {
-                                String activityName = dataSnapshot.child("activity_name").getValue(String.class).replace(",", " ");
-                                int hourDuration = Integer.parseInt(session_duration) / 60;
-                                int minutesDuration = Integer.parseInt(session_duration) % 60;
-                                String mnemonicPet = dataSnapshot.child("activity_pet").getValue(String.class);
-                                mySessions.add(new Session(session_id,
-                                        activity_id,
-                                        activityName,
-                                        new Time(hourDuration, minutesDuration, 0),
-                                        Integer.parseInt(session_day),
-                                        Integer.parseInt(session_month),
-                                        Integer.parseInt(session_year),
-                                        session_image,
-                                        mnemonicPet,
-                                        session_done)
-                                );
-                            } else {
-                                // L'activité n'existe pas dans la base de données
-                            }
-                        }
-
-                        @Override
-                        public void onCancelled(DatabaseError databaseError) {
-                            // Une erreur s'est produite lors de la récupération des données
-                        }
-                    });
+                    if(localDate.getDayOfMonth() == Integer.parseInt(session_day) && localDate.getYear() == Integer.parseInt(session_year) && localDate.getMonth().getValue() == Integer.parseInt(session_month)){
+                        mySessions.add("cc");
+                        Log.d("", "booom    : "+ mySessions);
+                    }
                 }
+                calendar = Calendar.getInstance();
+                int currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+                int currentMinute = calendar.get(Calendar.MINUTE);
+                if (currentHour == 18 ) {
+                    int sessionCount = 0;
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                        sessionCount = mySessions.size();
+                    }
+                    Log.d("", "baam    : "+ sessionCount);
+
+                    if (sessionCount > 0) {
+                        // Envoyer la notification
+                        showSessionNotification(sessionCount);
+                    }
+
+                }
+
+                // Planifier la prochaine vérification pour le lendemain à 10 heures
+
+
+
+                // Appeler la méthode de rappel pour notifier le chargement des sessions
+                //listener.onSessionsLoaded(mySessions);
             }
 
             @Override
@@ -159,16 +132,26 @@ public class BackgroundService extends Service {
                 Log.w("TAG", "Erreur lors de la récupération des données", databaseError.toException());
             }
         });
-
-        // Attendre la fin de l'exécution de la requête Firebase avant de retourner les sessions
-        try {
-            Thread.sleep(2000); // Attendre 2 secondes (ajustez selon vos besoins)
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        Log.d("sess" , "" +mySessions);
-
         return mySessions;
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        handler.post(runnable);
+        return START_STICKY;
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(runnable);
+    }
+
+    private void getS(){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference referenceActivity = database.getReference("Activity");
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        String uid = user.getUid();
+        ArrayList<Session> mySessions = new ArrayList<>();
     }
 
 

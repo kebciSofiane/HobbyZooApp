2233f7a9fce5ac19fdc396287787682e467d6a1a
Lina <lina.aitrazzouk@etu.univ-amu.fr>
2023-06-07 01:37:12 +0200

    .

--- app/src/main/java/com/example/hobbyzooapp/HomeActivity.java
+++ app/src/main/java/com/example/hobbyzooapp/HomeActivity.java
@@ -104,6 +104,7 @@ public class HomeActivity extends AppCompatActivity {
             showAnimals();
         });
 
+
         linearLayout1.setOnClickListener(v -> {
             Intent intent = new Intent(HomeActivity.this, ActivityPage.class);
             intent.putExtra("activity_id",(String) v.getTag());

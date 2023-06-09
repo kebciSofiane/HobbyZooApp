d3eba11320ee4153379d3ee69dac485de6ff235f
Lina <lina.aitrazzouk@etu.univ-amu.fr>
2023-06-09 14:02:19 +0200

    Merge Meriem (notification en cours)

---
+++ app/src/main/java/com/example/hobbyzooapp/OnSessionListRetrievedListener2.java
@@ -0,0 +1,7 @@
+package com.example.hobbyzooapp;
+
+import java.util.ArrayList;
+
+public interface OnSessionListRetrievedListener2 {
+    void onSessionListRetrieved(ArrayList<String> sessionList);
+}

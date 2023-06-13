6025d1564729c036edfbce6ee2822847bfdc1b45
f20008012 <clem.fresnel@gmail.com>
2023-06-13 15:44:53 +0200

    modif suppression

--- app/src/main/java/com/example/hobbyzooapp/AccountManagement/AccountActivity.java
+++ app/src/main/java/com/example/hobbyzooapp/AccountManagement/AccountActivity.java
@@ -11,13 +11,19 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.hobbyzooapp.Activities.ActivityPage;
+import com.example.hobbyzooapp.HomeActivity;
 import com.example.hobbyzooapp.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -30,9 +36,11 @@ public class AccountActivity extends AppCompatActivity {
     private FirebaseStorage storage;
     private StorageReference storageReference;
     private DatabaseReference databaseReference;
+    private FirebaseDatabase database;
     private static final int PICK_IMAGE = 1;
     private Uri imageUri;
     private boolean isEditMode = false;
+    private String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,9 +50,11 @@ public class AccountActivity extends AppCompatActivity {
         firebaseAuth = FirebaseAuth.getInstance();
         storage = FirebaseStorage.getInstance();
         storageReference = storage.getReference();
+        database = FirebaseDatabase.getInstance();
         databaseReference = FirebaseDatabase.getInstance().getReference("Users");
 
         FirebaseUser user = firebaseAuth.getCurrentUser();
+        userId = user.getUid();
 
         emailTv = findViewById(R.id.emailTv);
         changePasswordBtn = findViewById(R.id.changePasswordBtn);
@@ -116,6 +126,7 @@ public class AccountActivity extends AppCompatActivity {
         firebaseAuth.sendPasswordResetEmail(email)
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
+
                         Toast.makeText(AccountActivity.this, "Password reset email sent successfully", Toast.LENGTH_SHORT).show();
                         exitEditMode();
                     } else {
@@ -131,6 +142,7 @@ public class AccountActivity extends AppCompatActivity {
                     .addOnCompleteListener(task -> {
                         if (task.isSuccessful()) {
                             Toast.makeText(AccountActivity.this, "User unregistered successfully", Toast.LENGTH_SHORT).show();
+                            deleteDatas();
                         } else {
                             Toast.makeText(AccountActivity.this, "Failed to unregister user", Toast.LENGTH_SHORT).show();
                         }
@@ -144,5 +156,104 @@ public class AccountActivity extends AppCompatActivity {
         if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
             imageUri = data.getData();
         }
+    }
+
+    protected  void deleteDatas(){
+        DatabaseReference databaseReferenceSession = database.getReference("Session");
+        databaseReferenceSession.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    String user_id = dataSnapshot.child("user_id").getValue(String.class);
+                    String session_id = dataSnapshot.getKey();
+                    if(user_id != null) {
+                        if (user_id.equals(userId)) {
+                            databaseReferenceSession.child(session_id).removeValue();
+                        }
+                    }
+                }
+
+                DatabaseReference databaseReferenceActivity = database.getReference("Activity");
+                databaseReferenceActivity.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                            String user_id = dataSnapshot.child("user_id").getValue(String.class);
+                            String activity_id = dataSnapshot.getKey();
+                            if(user_id != null) {
+                                if (user_id.equals(userId)) {
+                                    deleteDatasTask(activity_id);
+                                    databaseReferenceActivity.child(activity_id).removeValue();
+                                }
+                            }
+                        }
+
+                        DatabaseReference databaseReferenceCategory = database.getReference("Category");
+                        databaseReferenceCategory.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot snapshot) {
+                                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                                    String user_id = dataSnapshot.child("user_id").getValue(String.class);
+                                    String category_id = dataSnapshot.getKey();
+                                    if(user_id != null) {
+                                        if (user_id.equals(userId)) {
+                                            databaseReferenceCategory.child(category_id).removeValue();
+                                        }
+                                    }
+                                }
+                                DatabaseReference databaseReferenceUsers = database.getReference("Users");
+                                databaseReferenceUsers.addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(DataSnapshot snapshot) {
+                                        for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                                            String user_id = dataSnapshot.getKey();
+                                            if(user_id != null) {
+                                                if (user_id.equals(userId)) {
+                                                    databaseReferenceUsers.child(user_id).removeValue();
+                                                }
+                                            }
+                                        }
+                                        startActivity(new Intent(AccountActivity.this, RegistrationOrConnexion.class));
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError error) {
+                                    }
+                                });
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                    }
+                });
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+
+    protected void deleteDatasTask(String activityID){
+        DatabaseReference databaseReferenceTasks = database.getReference("Tasks");
+        databaseReferenceTasks.orderByChild("activity_id").equalTo(activityID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    String task_id = dataSnapshot.getKey();
+                    if(task_id != null) {
+                        databaseReferenceTasks.child(task_id).removeValue();
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
     }
 }
